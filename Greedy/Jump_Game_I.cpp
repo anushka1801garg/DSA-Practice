1998fb7f645cbba444abf5b70fd750f5ae5057d6
Anushka Garg <anushka.garg1801@gmail.com>
2026-08-27 20:11:54 +0530

    Jump_Game_I.cpp Solution Added

--- Greedy/Jump_Game_I.cpp
+++ Greedy/Jump_Game_I.cpp
@@ -1,3 +1,20 @@
+/*
+Problem: Jump Game
+LeetCode: 55
+Link: https://leetcode.com/problems/jump-game/
+
+Approach:
+- Use a greedy approach to keep track of the farthest index that can be reached.
+- Maintain `maxi`, which represents the maximum reachable index so far.
+- Traverse the array from left to right.
+- If the current index `i` is greater than `maxi`, it means this index cannot be reached, so return `false`.
+- Otherwise, update `maxi` using `i + nums[i]` to find the farthest position reachable from the current index.
+- If all indices can be reached during the traversal, return `true`.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 #include<vector>
 using namespace std;
 
